6529fec65971b53d96f4e9b8a9c7c1f37a213200
FrameMuse <minicablestone@gmail.com>
2019-12-13 21:02:05 +0400

    Update 2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,43 @@
+// Joiners
+
+var Jwidth = $(".stdf2-game-joiners").width();
+var Jwidth_inner = $(".stdf2-game-joiners__inner").width();
+var Jwidth_limited = Jwidth - Jwidth_inner;
+var Jrate = (1000 / 120); // 120 UPS (почти FPS)
+var Joiners_work = true;
+if (Joiners_work) $(".stdf2-game-joiners__cover").on('mouseenter', function () {
+    var inner = $(".stdf2-game-joiners__inner");
+    this.interval = setInterval(() => {
+        var inner_indent = inner.css('margin-left').replace("px", "").intConvert();
+
+        if ($(this).hasClass("stdf2-game-joiners__cover--right")) {
+            var indent = (inner_indent - Jrate);
+        } else {
+            var indent = (inner_indent + Jrate);
+        }
+
+        if (indent < Jwidth_limited) {
+            indent = Jwidth_limited;
+            $(".stdf2-game-joiners__cover--right").css({ opacity: "0" });
+            $(".stdf2-game-joiners__cover--left").css({ opacity: "1" });
+        } else if (indent >= 0) {
+            indent = 0;
+            $(".stdf2-game-joiners__cover--right").css({ opacity: "1" });
+            $(".stdf2-game-joiners__cover--left").css({ opacity: "0" });
+        } else {
+            $(".stdf2-game-joiners__cover--right").css({ opacity: "1" });
+            $(".stdf2-game-joiners__cover--left").css({ opacity: "1" });
+        }
+
+        inner.css({
+            "margin-left": indent + "px",
+        });
+    }, Jrate);
+}).on('mouseleave', function () {
+    this.interval && clearInterval(this.interval);
+});
+
+
 // StandoffRoll Game
 
 class game_progress {
@@ -277,4 +317,12 @@ const stdf2_game = new class {
     }
 }
 
-$(window).on("load", () => stdf2_game.start());
+
+stdf2_game.__show("showing-winner");
+stdf2_game.edit("winner", {
+    nickname: "FrameMuse",
+    avatar: "assets/img/player4.png",
+    chance: 20,
+    ticket: 671,
+    benefit: 136782,
+});
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,47 +24,6 @@ function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Joiners
-
-var Jwidth = $(".stdf2-game-joiners").width();
-var Jwidth_inner = $(".stdf2-game-joiners__inner").width();
-var Jwidth_limited = Jwidth - Jwidth_inner;
-var Jrate = (1000 / 120); // 120 UPS (почти FPS)
-
-$(".stdf2-game-joiners__cover").on('mouseenter', function () {
-    var inner = $(".stdf2-game-joiners__inner");
-    this.interval = setInterval(() => {
-        var inner_indent = inner.css('margin-left').replace("px", "").intConvert();
-
-        if ($(this).hasClass("stdf2-game-joiners__cover--right")) {
-            var indent = (inner_indent - Jrate);
-        } else {
-            var indent = (inner_indent + Jrate);
-        }
-
-        if (indent < Jwidth_limited) {
-            indent = Jwidth_limited;
-            $(".stdf2-game-joiners__cover--right").css({ opacity: "0" });
-            $(".stdf2-game-joiners__cover--left").css({ opacity: "1" });
-        } else if (indent >= 0) {
-            indent = 0;
-            $(".stdf2-game-joiners__cover--right").css({ opacity: "1" });
-            $(".stdf2-game-joiners__cover--left").css({ opacity: "0" });
-        } else {
-            $(".stdf2-game-joiners__cover--right").css({ opacity: "1" });
-            $(".stdf2-game-joiners__cover--left").css({ opacity: "1" });
-        }
-
-        inner.css({
-            "margin-left": indent + "px",
-        });
-    }, Jrate);
-}).on('mouseleave', function () {
-    this.interval && clearInterval(this.interval);
-});
-
-
-
 // To top
 
 $(".to-top").click(() => $("html, body").animate({ scrollTop: 0 }, "slow"));
